d7de634f30943b6b01ff600f62fda9a5f737391f
Ali <ali.mirghafouri@gmail.com>
2020-11-10 09:33:03 +0800

    fix

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,24 +1,25 @@
-import React, {useState} from "react"
+import React from "react"
 import Carousel from "nuka-carousel"
 import {CAROUSEL} from "../../constant/const"
 import "./landing.css"
 import {IMAGES} from "../../image"
 
 export function Landing() {
-  const [autoPlay, setAutoPlay] = useState(true)
   // const bg_pic = {backgroundImage: `url(${images.home.bg_picture})`}
   return (
     <Carousel
+      speed={1000}
+      transitionMode="fade"
       className="wrapAround"
+      dragging={false}
       renderCenterLeftControls={() => null}
       renderCenterRightControls={() => null}
-      initialSlideHeight={1080}
-      autoplay={autoPlay}
+      initialSlideHeight={900}
+      autoplay={true}
+      pauseOnHover={true}
       wrapAround={true}>
       {CAROUSEL.map((text, index) => (
         <div
-          onMouseEnter={() => setAutoPlay(false)}
-          onMouseLeave={() => setAutoPlay(true)}
           className="Carousel_slides"
           style={{backgroundImage: `url(${IMAGES.landing.slides[index]})`}}>
           <div className="Carousel_slides_text">
