8bf39070696587b0b2867c4feadf2e55ff9ea00d
Ali <ali.mirghafouri@gmail.com>
2020-11-11 16:01:58 +0800

    slider update

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -3,6 +3,7 @@ import Carousel from "nuka-carousel"
 import {CAROUSEL} from "../../constant/const"
 import "./landing.css"
 import {IMAGES} from "../../image"
+import {AiOutlineLeft, AiOutlineRight} from "react-icons/ai"
 
 export function Landing() {
   return (
@@ -12,10 +13,14 @@ export function Landing() {
       transitionMode="fade"
       className="wrapAround"
       dragging={false}
-      renderCenterLeftControls={() => null}
-      renderCenterRightControls={() => null}
-      initialSlideHeight={900}
-      he
+      renderCenterLeftControls={({previousSlide}) => (
+        <AiOutlineLeft color="white" size="2vw" onClick={previousSlide} />
+      )}
+      renderCenterRightControls={({nextSlide}) => (
+        <AiOutlineRight color="white" size="2vw" onClick={nextSlide} />
+      )}
+      initialSlideHeight={800}
+      height
       autoplay={true}
       pauseOnHover={true}
       wrapAround={true}>
@@ -25,7 +30,7 @@ export function Landing() {
           style={{backgroundImage: `url(${IMAGES.landing.slides[index]})`}}>
           <div className="Carousel_slides_text">
             {text}
-            <button className="Carousel_slides_button">FREE DEMO</button>
+            <button className="Carousel_slides_button">VIEW FEATURES</button>
           </div>
         </div>
       ))}
