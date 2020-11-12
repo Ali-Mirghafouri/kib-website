27b5fde56f7ee6591887d847b4516cb25db185e8
Ali <ali.mirghafouri@gmail.com>
2020-11-12 10:31:55 +0800

    quick update

--- src/constant/const.js
+++ src/constant/const.js
@@ -1,5 +1,8 @@
-export const CAROUSEL = [
-  "Leverage on cutting-edge AI Chatbot to effortlessly connect with you customers & prospects",
-  "Optimise text or voice messaging success rates with contextual content, based on a customer’s behaviour and pattern.",
-  "Feel In-control with our Cockpit-like Dashboard showing key Analytics impacting your business",
-]
+export const HOME = {
+  ourClients: "Our Clients",
+  carousel: [
+    "Leverage on cutting-edge AI Chatbot to effortlessly connect with you customers & prospects",
+    "Optimise text or voice messaging success rates with contextual content, based on a customer’s behaviour and pattern.",
+    "Feel In-control with our Cockpit-like Dashboard showing key Analytics impacting your business",
+  ],
+}
--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,39 +1,43 @@
-import React from "react"
+import React, {Fragment} from "react"
 import Carousel from "nuka-carousel"
-import {CAROUSEL} from "../../constant/const"
+import {HOME} from "../../constant/const"
 import "./landing.css"
 import {IMAGES} from "../../image"
 import {AiOutlineLeft, AiOutlineRight} from "react-icons/ai"
 
 export function Landing() {
   return (
-    <Carousel
-      autoplayInterval={5000}
-      speed={2000}
-      transitionMode="fade"
-      className="wrapAround"
-      dragging={false}
-      renderCenterLeftControls={({previousSlide}) => (
-        <AiOutlineLeft color="white" size="2vw" onClick={previousSlide} />
-      )}
-      renderCenterRightControls={({nextSlide}) => (
-        <AiOutlineRight color="white" size="2vw" onClick={nextSlide} />
-      )}
-      initialSlideHeight={800}
-      height
-      autoplay={true}
-      pauseOnHover={true}
-      wrapAround={true}>
-      {CAROUSEL.map((text, index) => (
-        <div
-          className="Carousel_slides"
-          style={{backgroundImage: `url(${IMAGES.landing.slides[index]})`}}>
-          <div className="Carousel_slides_text">
-            {text}
-            <button className="Carousel_slides_button">VIEW FEATURES</button>
+    <Fragment>
+      <Carousel
+        autoplay={true}
+        autoplayInterval={5000}
+        className="wrapAround"
+        dragging={false}
+        height
+        initialSlideHeight={800}
+        pauseOnHover={true}
+        speed={2000}
+        transitionMode="fade"
+        wrapAround={true}
+        renderCenterLeftControls={({previousSlide}) => (
+          <AiOutlineLeft color="white" size="2vw" onClick={previousSlide} />
+        )}
+        renderCenterRightControls={({nextSlide}) => (
+          <AiOutlineRight color="white" size="2vw" onClick={nextSlide} />
+        )}>
+        {HOME.carousel.map((text, index) => (
+          <div
+            className="Carousel_slides"
+            style={{backgroundImage: `url(${IMAGES.landing.slides[index]})`}}>
+            <div className="Carousel_slides_text">
+              {text}
+              <button className="Carousel_slides_button">VIEW FEATURES</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </Carousel>
+        ))}
+      </Carousel>
+      <div className="our_clients_text">{HOME.ourClients}</div>
+      <div></div>
+    </Fragment>
   )
 }
